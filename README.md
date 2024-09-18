d2721ac1c146fc8a04f8cf9f3471f4b9f3dda378
Lup Yuen Lee <luppy@appkaki.com>
2024-09-18 14:32:58 +0800

    Add script to handle PRs every minute

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 See https://github.com/apache/nuttx/pull/13494#issuecomment-2357300091
 
 ```bash
+## See `run.sh`
 export GITHUB_TOKEN=...
 export GEMINI_API_KEY=...
 export RUST_LOG=info 
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+## Handle PRs every minute
+
+## Set the GitHub and Gemini Tokens
+## export GITHUB_TOKEN=...
+## export GEMINI_API_KEY=...
+. $HOME/github-token.sh
+. $HOME/gemini-token.sh
+
+## Echo commands
+set -x
+
+## Enable Rust Logging
+export RUST_LOG=info 
+export RUST_BACKTRACE=1
+
+## Handle PRs every minute
+for (( ; ; ))
+do
+    cargo run
+    sleep 60
+done
